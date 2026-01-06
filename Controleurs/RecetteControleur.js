0c81e6626d84783529f5d9e673f76908adb380f8
Jossnnier <fauconnierjosselin@yahoo.fr>
2026-01-06 14:52:29 +0100

    controleur

--- Controleurs/RecetteControleur.js
+++ Controleurs/RecetteControleur.js
@@ -35,3 +35,21 @@ exports.getRecetteById = async (req, res) => {
     res.status(404).json({ error: error.message });
   }
 };
+
+exports.updateRecette = async (req, res) => {
+    try {
+        await Recette.updateOne({_id: req.params.id},{... req.body});
+        res.status(200).json({message:'Recette mis à jour'});
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
+}
+
+exports.deleteRecette = async (req, res) => {
+    try {
+        await Recette.deleteOne({_id: req.params.id});
+        res.status(200).json({message: 'Recette supprimée'});
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
+}
