3bf5e4a280b85c7a98e984477bdde4efe257cf72
Jossnnier <fauconnierjosselin@yahoo.fr>
2026-01-06 09:36:02 +0100

    rajout nodemon

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+
 const app = express();
 const port = 3000;
+
+
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/cahier_de_recettes')
@@ -14,16 +17,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/cahier_de_recettes')
   });
 
 app.get('/', (req, res) => {
-  res.send('essaie node , express et mongoDB !');
+  res.send('API Cahier de Recettes est en ligne !');
 });
 
-app.get('/recettes', (req,res) =>{
-  res.send('Voici les recettes !');
-});
 
-app.get('/utilisateurs', (req,res)=>{
-  res.send('Voici les utilisateurs !');
-})
+app.use('/utilisateurs', utilisateurRoutes);
+app.use('/recettes', recetteRoutes);
 
 app.listen(port, () => {
   console.log(`Serveur démarré sur http://localhost:${port}`);
