82d48186529eace98403f34ad85bcf2e27fc1ee6
Jossnnier <fauconnierjosselin@yahoo.fr>
2026-01-11 17:54:12 +0100

    ok

--- configurationJS/gestionnaireErreur.js
+++ configurationJS/gestionnaireErreur.js
@@ -1,13 +1,13 @@
 const sendError = (res, error) => {
     console.error("LOG DEV : ", error);
 
-    // Erreurs de validation Mongoose 
+    
     if (error.name === 'ValidationError') {
         const messages = Object.values(error.errors).map(val => val.message);
         return res.status(400).json({ message: "Erreur de validation", erreurs: messages });
     }
 
-    // Erreurs de cast Mongoose 
+    
     if (error.name === 'CastError') {
         return res.status(400).json({ message: "Format de données invalide (ID malformé)" });
     }
